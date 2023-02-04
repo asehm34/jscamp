b29bbbf1f1092fa08638f592868f7f9c645806b9
asehm34 <61418800+asehm34@users.noreply.github.com>
2023-02-04 18:57:46 +0300

    Add files via upload

---
+++ task1q3/index.js
@@ -0,0 +1,40 @@
+//mükemmel sayı kendisi hariç pozitif tam bölenlerinin toplamı kendisine eşit olan sayıdır.
+
+//tam bölenlerinin toplamı
+
+function sumofDivisions(params) {
+    let a = [1]
+    for (let i = 2; i <= Math.ceil(params/2); i++) {
+        if (params%i===0) {
+            a.push(i)
+        } else {
+            continue
+        }
+        
+    }
+    //şimdi dizideki bölenlerin toplamını bulalım
+    let sum = 0;
+    a.forEach(element => {
+  sum += element;
+        
+    });
+    return sum;
+
+    
+}
+
+function isPerfect(param) {
+    if (sumofDivisions(param)===param) {
+        console.log(param +" is perfect number")
+        
+    }
+    else{
+        console.log(param +" is not perfect number")
+    }
+
+
+    
+}
+
+isPerfect(110)
+isPerfect(28)
---
+++ task1q3/readme.txt
@@ -0,0 +1,1 @@
+1000'e kadarki tüm mükemmel sayıları listeleyen programı yazınız
